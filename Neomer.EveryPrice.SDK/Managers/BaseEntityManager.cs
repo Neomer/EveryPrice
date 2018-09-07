2648975a7a0c93b78094dc55ab3f8ac92d4f9a74
Vinokurov Kirill <vinokurov@elewise.com>
2018-09-07 09:23:49 +0400

    master

---
+++ Neomer.EveryPrice.SDK/Managers/BaseEntityManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Neomer.EveryPrice.SDK.Models;
+
+namespace Neomer.EveryPrice.SDK.Managers
+{
+    public class BaseEntityManager<TEntity> : IEntityManager
+        where TEntity : IEntity
+    {
+        public IEntity Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Neomer.EveryPrice.SDK/Managers/IEntityManager.cs
@@ -0,0 +1,18 @@
+﻿using Neomer.EveryPrice.SDK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neomer.EveryPrice.SDK.Managers
+{
+    public interface IEntityManager
+    {
+        IEntity Get(Guid id);
+
+        void Remove(Guid id);
+
+        void Save(Guid id);
+    }
+}
