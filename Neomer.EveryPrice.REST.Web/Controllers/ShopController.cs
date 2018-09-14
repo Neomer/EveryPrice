327ff8c2ce8048e323eecff96554b15a4a279d3c
Neomer <vin.kirill@gmail.com>
2018-09-14 12:46:23 +0400

    feature/CreatingShops

--- Neomer.EveryPrice.REST.Web/Controllers/ShopController.cs
+++ Neomer.EveryPrice.REST.Web/Controllers/ShopController.cs
@@ -1,4 +1,5 @@
-﻿using Neomer.EveryPrice.SDK.Models;
+﻿using Neomer.EveryPrice.REST.Web.Models;
+using Neomer.EveryPrice.SDK.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,14 +11,35 @@ namespace Neomer.EveryPrice.REST.Web.Controllers
 {
     public class ShopController : ApiController
     {
-        public Shop Get()
+        public Shop Get(double lat, double lng, double distance)
         {
-
+            return null;
         }
 
         public Shop Get(Guid id)
         {
+            return null;
+        }
 
+        /// <summary>
+        /// Редактирование информации о мазагазине
+        /// </summary>
+        /// <param name="id">Идентификатор</param>
+        /// <param name="editModel">Модель данных</param>
+        /// <returns></returns>
+        public Shop Post(Guid id, [FromBody]ShopEditModel editModel)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Добавляем новый магазин
+        /// </summary>
+        /// <param name="editModel"></param>
+        /// <returns></returns>
+        public Shop Put([FromBody]ShopEditModel editModel)
+        {
+            return null;
         }
 
     }
