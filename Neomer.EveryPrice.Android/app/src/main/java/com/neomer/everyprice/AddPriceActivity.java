46fb99495c0c0d0e9c248e9b30ea4476f95ee8df
Neomer <vin.kirill@gmail.com>
2018-09-24 22:55:00 +0400

    develop

---
+++ Neomer.EveryPrice.Android/app/src/main/java/com/neomer/everyprice/AddPriceActivity.java
@@ -0,0 +1,13 @@
+package com.neomer.everyprice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddPriceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_price);
+    }
+}
--- Neomer.EveryPrice.Android/app/src/main/java/com/neomer/everyprice/ShopDetailsActivity.java
+++ Neomer.EveryPrice.Android/app/src/main/java/com/neomer/everyprice/ShopDetailsActivity.java
@@ -1,9 +1,15 @@
 package com.neomer.everyprice;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,8 +22,12 @@ import java.util.List;
 
 public class ShopDetailsActivity extends AppCompatActivity {
 
+    public final static int RESULT_FOR_ADD_PRICE = 0;
+
     private Shop shop;
-    ShopDetailsRecyclerViewAdapter shopDetailsRecyclerViewAdapter;
+    private ShopDetailsRecyclerViewAdapter shopDetailsRecyclerViewAdapter;
+    private FloatingActionButton floatingActionButton;
+    private boolean actionsShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +42,85 @@ public class ShopDetailsActivity extends AppCompatActivity {
         TextView tvName = findViewById(R.id.shopDetails_tvName);
         tvName.setText(shop.getName());
 
+        floatingActionButton = findViewById(R.id.shopDetails_floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showActions();
+            }
+        });
+
         setupRecyclerView();
         startLoadProducts();
 
+    }
+
+    private void showActions() {
+        actionsShow = !actionsShow;
+
+        FloatingActionButton fabAddPrice = findViewById(R.id.shopDetails_addPrice);
+        fabAddPrice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToAddPriceActivity();
+            }
+        });
+
+        FloatingActionButton fabEditInfo = findViewById(R.id.shopDetails_editShopInformation);
+        fabEditInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToEditInfoActivity();
+            }
+        });
+
+        if (actionsShow) {
+            Animation show_fab = AnimationUtils.loadAnimation(getApplication(), R.anim.action_fab_show);
+
+            ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) fabAddPrice.getLayoutParams();
+            layoutParams.rightMargin += (int) (floatingActionButton.getWidth());
+            layoutParams.bottomMargin += (int) (floatingActionButton.getHeight());
+            fabAddPrice.setLayoutParams(layoutParams);
+            fabAddPrice.startAnimation(show_fab);
+            fabAddPrice.setClickable(true);
+
+            layoutParams = (ConstraintLayout.LayoutParams) fabEditInfo.getLayoutParams();
+            layoutParams.rightMargin += (int) (1.33 * floatingActionButton.getWidth());
+            layoutParams.bottomMargin -= (int) (0.12 * floatingActionButton.getHeight());
+            fabEditInfo.setLayoutParams(layoutParams);
+            fabEditInfo.startAnimation(show_fab);
+            fabEditInfo.setClickable(true);
+
+        } else {
+            Animation hide_fab = AnimationUtils.loadAnimation(getApplication(), R.anim.action_fab_hide);
+
+            ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) fabAddPrice.getLayoutParams();
+            layoutParams.rightMargin -= (int) (floatingActionButton.getWidth());
+            layoutParams.bottomMargin -= (int) (floatingActionButton.getHeight());
+            fabAddPrice.setLayoutParams(layoutParams);
+            fabAddPrice.startAnimation(hide_fab);
+            fabAddPrice.setClickable(false);
+
+            layoutParams = (ConstraintLayout.LayoutParams) fabEditInfo.getLayoutParams();
+            layoutParams.rightMargin -= (int) (1.33 * floatingActionButton.getWidth());
+            layoutParams.bottomMargin += (int) (0.12 * floatingActionButton.getHeight());
+            fabEditInfo.setLayoutParams(layoutParams);
+            fabEditInfo.startAnimation(hide_fab);
+            fabEditInfo.setClickable(false);
+        }
+        
+    }
+
+    private void moveToEditInfoActivity() {
+        Intent intent = new Intent(this, AddPriceActivity.class);
+        intent.putExtra(Shop.class.getCanonicalName(), shop);
+        startActivityForResult(intent, RESULT_FOR_ADD_PRICE);
+    }
+
+    private void moveToAddPriceActivity() {
+        Intent intent = new Intent(this, AddPriceActivity.class);
+        intent.putExtra(Shop.class.getCanonicalName(), shop);
+        startActivityForResult(intent, RESULT_FOR_ADD_PRICE);
     }
 
     private void startLoadProducts() {
