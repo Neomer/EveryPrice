86c69fd715d159131d8fcd90366c76be39e359d9
Vinokurov Kirill <vinokurov@elewise.com>
2018-10-28 19:33:24 +0400

    feature/#90

---
+++ Neomer.EveryPrice.Android/app/src/main/java/com/neomer/everyprice/core/helpers/SecurityHelper.java
@@ -0,0 +1,28 @@
+package com.neomer.everyprice.core.helpers;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import com.neomer.everyprice.MainActivity;
+import com.neomer.everyprice.api.WebApiFacade;
+
+public final class SecurityHelper {
+
+    public final static String APP_PREFERENCES = "EveryPricePreferences";
+    public final static String APP_PREFERENCES_TOKEN = "Token";
+
+    /**
+     * Удаляет информацию о сохраненном токене
+     */
+    public static void ClearSavedToken(Context context) {
+        if (context == null) {
+            return;
+        }
+        SharedPreferences preferences = context.getSharedPreferences(SecurityHelper.APP_PREFERENCES, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(SecurityHelper.APP_PREFERENCES_TOKEN, null);
+        editor.apply();
+    }
+
+}
